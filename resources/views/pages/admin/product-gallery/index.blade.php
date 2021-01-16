d918656cc376866079a17f4318487a14c6fd5012
agung1997-mhs <agungkemayoran97@gmail.com>
2021-01-16 17:54:42 +0700

    initial commit

---
+++ resources/views/pages/admin/product-gallery/index.blade.php
@@ -0,0 +1,144 @@
+@extends('layouts.admin')
+
+@section('title')
+    Product Gallery
+@endsection
+
+@section('content')
+<!-- Page Content -->
+<div id="page-content-wrapper">
+    <nav
+        class="navbar navbar-expand-lg navbar-light navbar-store fixed-top"
+        data-aos="fade-down"
+        aria-label=""
+    >
+        <div class="container-fluid">
+            <button
+                class="btn btn-secondary d-md-none mr-auto mr-2"
+                id="menu-toggle"
+            >
+                &laquo; Menu
+            </button>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div
+                class="collapse navbar-collapse"
+                id="navbarSupportedContent"
+            >
+                <!-- Desktop Menu  -->
+                <ul class="navbar-nav d-none d-lg-flex ml-auto">
+                    <li class="nav-item dropdown">
+                        <a
+                            href="#"
+                            class="nav-link"
+                            id="navbarDropdown"
+                            role="button"
+                            data-toggle="dropdown"
+                        >
+                            <img
+                                src="/images/icon-user.png"
+                                alt=""
+                                class="rounded-circle mr-2 profile-picture"
+                            />
+                            Hi, Agung
+                        </a>
+                        <div class="dropdown-menu">
+                            <a href="/" class="dropdown-item"
+                                >Logout</a
+                            >
+                        </div>
+                    </li>
+                </ul>
+
+                <!-- Mobile Menu -->
+                <ul class="navbar-nav d-block d-lg-none">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link"
+                            >Hi, Agung</a
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <a
+                            href=""
+                            class="nav-link d-inline-block"
+                            >Cart</a
+                        >
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Section Content -->
+    <div
+        class="section-content section-dashboard-home"
+        data-aos="fade-up"
+    >
+        <div class="container-fluid">
+            <div class="dashboard-heading">
+                <h2 class="dashboard-title">Product Gallery</h2>
+                <p class="dashboard-subtitle">
+                    List of Product Gallery
+                </p>
+            </div>
+            <div class="dashboard-content">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <a href="{{ route('product-gallery.create') }}" class="btn btn-primary mb-3">
+                                    + Tambah Product Gallery Baru
+                                </a>
+                                <div class="table table-responsive">
+                                    <table class="table table-hover scroll-horizontal-vertical w-100" id="crudTable">
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Produk</th>
+                                                <th>Foto</th>
+                                                <th>Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody></tbody>
+                                    </table>
+                                </div>  
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('addon-script')
+    <script>
+        var datatable = $('#crudTable').DataTable({
+            processing: true,
+            serverSide: true,
+            ordering: true,
+            ajax: {
+                url: '{!! url()->current() !!}',
+            },
+            columns: [
+                { data: 'id', name: 'id' },
+                { data: 'product.name', name: 'product.name' },
+                { data: 'photos', name: 'photos' },
+                {
+                    data: 'action',
+                    name: 'action',
+                    orderable: false,
+                    searcable: false,
+                    width: '15%'
+                }, 
+            ]
+        });
+    </script>
+@endpush
---
+++ resources/views/pages/admin/user/create.blade.php
@@ -0,0 +1,151 @@
+@extends('layouts.admin')
+
+@section('title')
+    User
+@endsection
+
+@section('content')
+<!-- Page Content -->
+<div id="page-content-wrapper">
+    <nav
+        class="navbar navbar-expand-lg navbar-light navbar-store fixed-top"
+        data-aos="fade-down"
+        aria-label=""
+    >
+        <div class="container-fluid">
+            <button
+                class="btn btn-secondary d-md-none mr-auto mr-2"
+                id="menu-toggle"
+            >
+                &laquo; Menu
+            </button>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div
+                class="collapse navbar-collapse"
+                id="navbarSupportedContent"
+            >
+                <!-- Desktop Menu  -->
+                <ul class="navbar-nav d-none d-lg-flex ml-auto">
+                    <li class="nav-item dropdown">
+                        <a
+                            href="#"
+                            class="nav-link"
+                            id="navbarDropdown"
+                            role="button"
+                            data-toggle="dropdown"
+                        >
+                            <img
+                                src="/images/icon-user.png"
+                                alt=""
+                                class="rounded-circle mr-2 profile-picture"
+                            />
+                            Hi, Agung
+                        </a>
+                        <div class="dropdown-menu">
+                            <a href="/" class="dropdown-item"
+                                >Logout</a
+                            >
+                        </div>
+                    </li>
+                </ul>
+
+                <!-- Mobile Menu -->
+                <ul class="navbar-nav d-block d-lg-none">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link"
+                            >Hi, Agung</a
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <a
+                            href=""
+                            class="nav-link d-inline-block"
+                            >Cart</a
+                        >
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Section Content -->
+    <div
+        class="section-content section-dashboard-home"
+        data-aos="fade-up"
+    >
+        <div class="container-fluid">
+            <div class="dashboard-heading">
+                <h2 class="dashboard-title">User</h2>
+                <p class="dashboard-subtitle">
+                    Create New User 
+                </p>
+            </div>
+            <div class="dashboard-content">
+                <div class="row">
+                    <div class="col-md-12">
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <div class="card">
+                            <div class="card-body">
+                                <form action="{{ route('user.store') }}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Nama User</label>
+                                                <input type="text" name="name" class="form-control" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Email User</label>
+                                                <input type="email" name="email" class="form-control" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Password User</label>
+                                                <input type="password" name="password" class="form-control" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Roles</label>
+                                                <select name="roles" required class="form-control">
+                                                    <option value="ADMIN">Admin</option>
+                                                    <option value="USER">User</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col text-right">
+                                            <button type="submit" class="btn btn-success px-5">
+                                                Save Now
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
